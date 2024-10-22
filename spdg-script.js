08267248614dbcdceb4c7f3aba6b179ede46ba10
yamitools <147960568+yamitools@users.noreply.github.com>
2024-10-22 01:22:13 +0100

    Update spdg-script.js
    debug the ajax request

--- spdg-script.js
+++ spdg-script.js
@@ -5,6 +5,9 @@ jQuery(document).ready(function($) {
         var productName = $('#spdg_product_name').val();
         var productFeatures = $('#spdg_product_features').val();
 
+        console.log('Product Name: ', productName);
+        console.log('Product Features: ', productFeatures);
+
         $.ajax({
             url: ajaxurl,  // WordPress-specific variable that points to admin-ajax.php
             type: 'POST',
@@ -14,9 +17,11 @@ jQuery(document).ready(function($) {
                 product_features: productFeatures
             },
             success: function(response) {
+                console.log('Response from server: ', response);
                 $('#spdg_generated_description').val(response);
             },
-            error: function() {
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log('AJAX Error: ', textStatus, errorThrown);
                 alert('Failed to generate description. Please try again.');
             }
         });
